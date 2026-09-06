348a8316b6485194a2df816e836e3252ed5104e5
xavio2495 <xavio.delta2495@gmail.com>
2026-09-06 04:16:30 +0000

    web: scratch files from another lane were swept into a commit

--- web/.tmp/t.ts
+++
@@ -1,7 +0,0 @@
-import { reachableFrom } from "../scripts/module-graph.ts";
-import { writeFileSync, mkdirSync, rmSync } from "node:fs";
-mkdirSync("/tmp/g", { recursive: true });
-writeFileSync("/tmp/g/a.ts", 'import type { X } from "./b.ts";\nexport const q = 1;\n');
-writeFileSync("/tmp/g/b.ts", 'export type X = number;\n');
-console.log("type-only import followed?", [...reachableFrom(["/tmp/g/a.ts"])].length === 2);
-rmSync("/tmp/g", { recursive: true });
